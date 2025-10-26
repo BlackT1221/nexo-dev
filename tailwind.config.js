21a234fb871e544a3dbcc9a3414a5698d7b4720b
BlackT1221 <cdmartinez1121@gmail.com>
2025-10-26 15:09:30 -0500

    Primera version de software solutions. Astro + Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#F58A07",
+      },
+    },
+  },
+  plugins: [],
+};
